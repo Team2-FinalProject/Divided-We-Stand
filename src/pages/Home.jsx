aa86dffb65faad1e681f35f4bed3d3f4eb124ef1
brotheryeska <yeskahaganta3838@gmail.com>
2020-12-12 09:16:59 +0700

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
-    return (
-        <Container>
-            Homepage
-        </Container>
-    )
+  return (
+    <Container>
+        <div classNameName="nes-field">
+          <label for="name_field">Your name</label>
+          <input type="text" id="name_field" className="nes-input" />
+        </div>
+        <button
+          type="button"
+          className="nes-btn is-success"
+          style={{ width: 200 }}
+        >
+          Start
+        </button>
+    </Container>
+  );
 }
