259e1bdc6ebee831aa126c9603da9bedda1652f0
brotheryeska <yeskahaganta3838@gmail.com>
2020-12-13 22:33:31 +0700

    lobbypage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import "../App.css";
 
 export default function Home() {
   return (
-    <Container>
-        <div classNameName="nes-field">
-          <label for="name_field">Your name</label>
-          <input type="text" id="name_field" className="nes-input" />
-        </div>
-        <button
-          type="button"
-          className="nes-btn is-success"
-          style={{ width: 200 }}
-        >
-          Start
-        </button>
-    </Container>
+    <div className="mainpage">
+      <div className="nes-field">
+        <label for="name_field">Your name</label>
+        <input type="text" id="name_field" className="nes-input" />
+      </div>
+      <button
+        type="button"
+        className="nes-btn is-success"
+        style={{ width: 200 }}
+      >
+        Start
+      </button>
+    </div>
   );
 }
